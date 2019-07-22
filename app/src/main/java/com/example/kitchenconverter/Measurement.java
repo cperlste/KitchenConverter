cf861c9a10f56f13689e5e94b31aa2ed88be07ac
cperlste <52978328+cperlste@users.noreply.github.com>
2019-07-22 16:45:20 -0400

    added measurement class

---
+++ app/src/main/java/com/example/kitchenconverter/Measurement.java
@@ -0,0 +1,17 @@
+package com.example.kitchenconverter;
+
+public class Measurement {
+    double cup;
+    double tbsp;
+    double tsp;
+    double oz;
+    public Measurement(){
+        cup=0.0;
+        tbsp=0.0;
+        tsp=0.0;
+        oz=0.0;
+    }
+    public void setCup(double cups){
+        cup=cups;
+    }
+}
