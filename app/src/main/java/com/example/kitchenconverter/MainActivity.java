9af5d2101fa7f56bc6fa3060af840a5e8b7bdd95
cperlste <52978328+cperlste@users.noreply.github.com>
2019-07-23 21:37:02 -0400

    finished main activity

--- app/src/main/java/com/example/kitchenconverter/MainActivity.java
+++ app/src/main/java/com/example/kitchenconverter/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.AdapterView.*;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,7 +19,7 @@ import com.google.android.material.snackbar.Snackbar;
 import java.text.DecimalFormat;
 
 
-public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     private Spinner mSpinnerFraction, mSpinnerMeasurement, mSpinnerMeasurement2;
     private double mFrom, mTo;
     private EditText wholeNumber;
@@ -41,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         Log.d("SETUP", "setting up third spinner");
         setUpSpinner3();
     }
+
     private void initializeFields() {
         Log.d("SETUP", "creating measurement object");
         mMeasurement = new Measurement();
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         String[] fractions = new String[]{"none", "3/4", "2/3", "1/2", "1/3", "1/4", "1/8", "1/16"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, fractions);
         mSpinnerFraction.setAdapter(adapter);
-        mSpinnerFraction.setOnItemSelectedListener(this);
     }
 
     private void setUpSpinner2() {
@@ -67,125 +66,122 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         String[] measurements = new String[]{"cup", "teaspoons", "tablespoons", "ounces"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, measurements);
         mSpinnerMeasurement2.setAdapter(adapter);
-        mSpinnerMeasurement2.setOnItemSelectedListener(this);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        Log.d("CALC", "calculating mFrom based on whole number and fraction");
-        if (view.equals(mSpinnerFraction)){
-            if (i==0){
-                mFrom=Double.parseDouble(wholeNumber.getText().toString());
-            }
-            else {
-                String strFraction = (String) adapterView.getItemAtPosition(i);
-                double decimal = convertToDecimal(strFraction);
-                Log.d("CONV_RECIPE", "Decimal: " + decimal);
-                mFrom = Integer.parseInt(wholeNumber.getText().toString()) + decimal;
-            }
-        }
-        else if(view.equals(mSpinnerMeasurement)){
-            typeFrom= (String) adapterView.getItemAtPosition(i);
-        }
-        else if(view.equals(mSpinnerMeasurement2)){
-            typeTo= (String) adapterView.getItemAtPosition(i);
-        }
-    }
 
     private double convertToDecimal(String strFraction) {
-        double quotient = strFraction.charAt(0) / strFraction.charAt(2);
-        return quotient;
+        int numerator = strFraction.charAt(0);
+        int denominator = strFraction.charAt(2);
+        return (double) numerator / denominator;
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-        typeFrom="cup";
-        typeTo="tbsp";
-    }
-    public void calcResults(View view){
-        Log.d("CALC", "type to: "+typeTo+" typeFrom "+typeFrom+" mFrom "+mFrom+" mTo: "+mTo);
-        if (mFrom==0.0){
+    public void calcResults(View view) {
+        Log.d("CALC", "getting typeFrom from Spinner");
+        String typeFrom = mSpinnerMeasurement.getSelectedItem().toString();
+        Log.d("CALC", "type From is now "+typeFrom);
+        Log.d("CALC", "getting typeTo from Spinner");
+        String typeTo = mSpinnerMeasurement2.getSelectedItem().toString();
+        Log.d("CALC", "typeTo is now "+typeTo);
+        Log.d("CALC", "getting string fraction from spinner");
+        String strFraction = mSpinnerFraction.getSelectedItem().toString();
+        Log.d("CALC", "strFraction is now "+strFraction);
+        Log.d("CALC", "converting string fraction to decimal. Calling convertToDecimal method");
+        double decimal = convertToDecimal(strFraction);
+        Log.d("CALC", "fraction decimal value "+decimal);
+        Log.d("CALC", "reading the whole number as a string from wholeNumber edit text");
+        String strWholeNum = wholeNumber.getText().toString();
+        Log.d("CALC", "converting the string to a double");
+        double numDouble = Double.parseDouble(strWholeNum);
+        Log.d("CALC", "numDouble is now "+numDouble);
+        Log.d("CALC", "adding numDouble and decimal");
+        double mFrom = numDouble + decimal;
+        Log.d("CALC", "mFrom is now"+mFrom);
+        Log.d("CALC", "type to: " + typeTo + " typeFrom " + typeFrom + " mFrom " + mFrom + " mTo: " + mTo);
+        if (mFrom == 0.0) {
             Snackbar.make(view, "The measurement cannot be empty.", Snackbar.LENGTH_LONG).show();
-        }
-        else{
-            ifAndElse();
-            String msg= mFrom+ " "+typeFrom+ " is "+ mTo+" "+ typeTo+".";
-            Intent intent;
+        } else {
+            ifAndElse(mFrom);
+            String msg = mFrom + " " + typeFrom + " is " + mTo + " " + typeTo + ".";
+            //Intent intent;
             Snackbar.make(view, msg, Snackbar.LENGTH_LONG).show();
-                    //.setAction("More",{
-                //intent = new Intent(getApplicationContext(), ResultsActivity.class);
-                //intent.putExtra("NUM_FROM", mFrom);
-                //intent.putExtra("NUM_TO", mTo);
+            //.setAction("More",{
+            //intent = new Intent(getApplicationContext(), ResultsActivity.class);
+            //intent.putExtra("NUM_FROM", mFrom);
+            //intent.putExtra("NUM_TO", mTo);
             //}).show();
         }
     }
 
-    private void ifAndElse() {
+    private void ifAndElse(double from) {
         //cup to oz
-        if(typeFrom.equalsIgnoreCase("cup")&& typeTo.equalsIgnoreCase("oz")){
-            mTo=mMeasurement.cupToOz(mFrom);
+        if (typeFrom.equalsIgnoreCase("cup") && typeTo.equalsIgnoreCase("oz")) {
+            mMeasurement.setCup(from);
+            mTo = mMeasurement.cupToOz(from);
+            mMeasurement.setOz(mTo);
         }
         //oz to cup
-        else if(typeFrom.equalsIgnoreCase("oz")&& typeTo.equalsIgnoreCase("cup")){
-            mTo=mMeasurement.ozToCup(mFrom);
+        else if (typeFrom.equalsIgnoreCase("oz") && typeTo.equalsIgnoreCase("cup")) {
+            mTo = mMeasurement.ozToCup(from);
         }
         //cup to tbsp
-        else if(typeFrom.equalsIgnoreCase("cup")&& typeTo.equalsIgnoreCase("tbsp")){
-            mTo=mMeasurement.cupToTbsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("cup") && typeTo.equalsIgnoreCase("tbsp")) {
+            mTo = mMeasurement.cupToTbsp(from);
         }
         //cup to tsp
-        else if(typeFrom.equalsIgnoreCase("cup")&& typeTo.equalsIgnoreCase("tsp")){
-            mTo=mMeasurement.cupToTsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("cup") && typeTo.equalsIgnoreCase("tsp")) {
+            mTo = mMeasurement.cupToTsp(from);
         }
         //oz to tbsp
-        else if(typeFrom.equalsIgnoreCase("oz")&& typeTo.equalsIgnoreCase("tbsp")){
-            mTo=mMeasurement.ozToTbsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("oz") && typeTo.equalsIgnoreCase("tbsp")) {
+            mTo = mMeasurement.ozToTbsp(from);
         }
         //oz to tsp
-        else if(typeFrom.equalsIgnoreCase("oz")&& typeTo.equalsIgnoreCase("tsp")){
-            mTo=mMeasurement.ozToTsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("oz") && typeTo.equalsIgnoreCase("tsp")) {
+            mTo = mMeasurement.ozToTsp(from);
         }
         //tbsp to cup
-        else if(typeFrom.equalsIgnoreCase("tbsp")&& typeTo.equalsIgnoreCase("cup")){
-            mTo=mMeasurement.tbspToCup(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tbsp") && typeTo.equalsIgnoreCase("cup")) {
+            mTo = mMeasurement.tbspToCup(from);
         }
         //tsp to cup
-        else if(typeFrom.equalsIgnoreCase("tsp")&& typeTo.equalsIgnoreCase("cup")){
-            mTo=mMeasurement.tspToCup(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tsp") && typeTo.equalsIgnoreCase("cup")) {
+            mTo = mMeasurement.tspToCup(from);
         }
         //tbsp to oz
-        else if(typeFrom.equalsIgnoreCase("tbsp")&& typeTo.equalsIgnoreCase("oz")){
-            mTo=mMeasurement.tbspToOz(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tbsp") && typeTo.equalsIgnoreCase("oz")) {
+            mTo = mMeasurement.tbspToOz(from);
         }
         //tsp to oz
-        else if(typeFrom.equalsIgnoreCase("tsp")&& typeTo.equalsIgnoreCase("oz")){
-            mTo=mMeasurement.tspToOz(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tsp") && typeTo.equalsIgnoreCase("oz")) {
+            mTo = mMeasurement.tspToOz(from);
         }
         //tsp to tbsp
-        else if (typeFrom.equalsIgnoreCase("tsp")&& typeTo.equalsIgnoreCase("tbsp")){
-            mTo=mMeasurement.tspToTbsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tsp") && typeTo.equalsIgnoreCase("tbsp")) {
+            mTo = mMeasurement.tspToTbsp(from);
 
         }
         //tbsp to tsp
-        else if(typeFrom.equalsIgnoreCase("tbsp")&& typeTo.equalsIgnoreCase("tsp")){
-            mTo=mMeasurement.tbspToTsp(mFrom);
+        else if (typeFrom.equalsIgnoreCase("tbsp") && typeTo.equalsIgnoreCase("tsp")) {
+            mTo = mMeasurement.tbspToTsp(from);
         }
     }
+
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putDouble("AMT_FROM",mFrom);
+        outState.putDouble("AMT_FROM", mFrom);
         outState.putDouble("AMT_TO", mTo);
         outState.putString("TYPE_FROM", typeFrom);
         outState.putString("TYPE_TO", typeTo);
     }
+
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState){
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        mFrom=savedInstanceState.getDouble("AMT_FROM");
-        mTo=savedInstanceState.getDouble("AMT_TO");
-        typeFrom=savedInstanceState.getString("TYPE_FROM");
-        typeTo=savedInstanceState.getString("TYPE_TO");
+        mFrom = savedInstanceState.getDouble("AMT_FROM");
+        mTo = savedInstanceState.getDouble("AMT_TO");
+        typeFrom = savedInstanceState.getString("TYPE_FROM");
+        typeTo = savedInstanceState.getString("TYPE_TO");
 
     }
 
--- app/src/main/java/com/example/kitchenconverter/Measurement.java
+++ app/src/main/java/com/example/kitchenconverter/Measurement.java
@@ -1,21 +1,21 @@
 package com.example.kitchenconverter;
 
 public class Measurement {
-    double cup;
-    double tbsp;
-    double tsp;
-    double oz;
-    public Measurement(){
-        cup=0.0;
-        tbsp=0.0;
-        tsp=0.0;
-        oz=0.0;
+    private double cup, tbsp, tsp, oz;
+
+    public Measurement() {
+        cup = 0.0;
+        tbsp = 0.0;
+        tsp = 0.0;
+        oz = 0.0;
     }
-    public void setCup(double cups){
-        cup=cups;
+
+    public void setCup(double cups) {
+        cup = cups;
     }
-    public void setTbsp(double tbsps){
-        tbsp=tbsps;
+
+    public void setTbsp(double tbsps) {
+        tbsp = tbsps;
     }
 
     public void setTsp(double tsp) {
@@ -41,40 +41,52 @@ public class Measurement {
     public double getOz() {
         return oz;
     }
-    public double ozToCup(double oz){
-        return oz/8;
+
+    public double ozToCup(double oz) {
+        return oz / 8;
     }
-    public double cupToOz(double cup){
-        return cup*8;
+
+    public double cupToOz(double cup) {
+        return cup * 8;
     }
-    public double cupToTbsp(double cup){
-        return cup*16;
+
+    public double cupToTbsp(double cup) {
+        return cup * 16;
     }
-    public double tbspToCup(double tbsp){
-        return tbsp/16;
+
+    public double tbspToCup(double tbsp) {
+        return tbsp / 16;
     }
-    public double cupToTsp(double cup){
-        return cup*48;
+
+    public double cupToTsp(double cup) {
+        return cup * 48;
     }
-    public double tspToCup(double tsp){
-        return tsp/48;
+
+    public double tspToCup(double tsp) {
+        return tsp / 48;
     }
-    public double ozToTbsp(double oz){
-        return oz*2;
+
+    public double ozToTbsp(double oz) {
+        return oz * 2;
     }
-    public double tbspToOz(double tbsp){
-        return tbsp/2;
+
+    public double tbspToOz(double tbsp) {
+        return tbsp / 2;
     }
-    public double ozToTsp(double oz){
-        return oz*6;
+
+    public double ozToTsp(double oz) {
+        return oz * 6;
     }
-    public double tspToOz(double tsp){
-        return tsp/6;
+
+    public double tspToOz(double tsp) {
+        return tsp / 6;
     }
-    public double tspToTbsp(double tsp){
-        return tsp/3;
+
+    public double tspToTbsp(double tsp) {
+        return tsp / 3;
     }
-    public double tbspToTsp(double tbsp){
-        return tbsp*3;
+
+    public double tbspToTsp(double tbsp) {
+        return tbsp * 3;
     }
 }
