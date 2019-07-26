1a251cf96e51b8cad9f61b61eb9b902c1ebfc9f6
cperlste <52978328+cperlste@users.noreply.github.com>
2019-07-25 20:25:28 -0400

    updates including FAB

--- app/src/main/java/com/example/kitchenconverter/ChartActivity.java
+++ app/src/main/java/com/example/kitchenconverter/ChartActivity.java
@@ -3,12 +3,29 @@ package com.example.kitchenconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import static com.google.android.material.snackbar.Snackbar.LENGTH_LONG;
 
 public class ChartActivity extends AppCompatActivity {
- public ChartActivity(){}
+    public ChartActivity() {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
+
 }
